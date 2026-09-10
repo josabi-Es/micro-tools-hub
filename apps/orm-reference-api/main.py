76c45b57a204b98c2b09ca5cb81c073d41107fae
josabi-Es <josabiedma2@gmail.com>
2026-09-10 18:34:16 +0200

    style: ruff format minor

--- apps/orm-reference-api/main.py
+++ apps/orm-reference-api/main.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Depends, FastAPI, HTTPException, Query
-from utils.logger import logger
 from models.database import POSTGRES_DB, POSTGRES_HOST, POSTGRES_PORT, get_db, get_engine
 from sqlalchemy import select, text
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 from utils.auth import verify_api_key
+from utils.logger import logger
 from utils.reflection import (
     find_fk_relation,
     get_primary_key_columns,
--- apps/orm-reference-api/models/database.py
+++ apps/orm-reference-api/models/database.py
@@ -1,9 +1,9 @@
 import os
 
 from dotenv import load_dotenv
-from utils.logger import logger
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from utils.logger import logger
 
 load_dotenv()
 
